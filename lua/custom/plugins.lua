77ca01062e28e0062bb8c263e76b4b4d7eadf03d
DotNaos <schuetzoliver00@gmail.com>
2023-08-30 13:27:29 +0000

    Adding Copilot

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,16 @@
 local plugins = { 
-  {
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+    {
+      'github/copilot.vim'
+    },
+    {
+      'numToStr/Comment.nvim',
+      opts = {
+        -- add any options here
+      },
+      lazy = false,
+    },
+    {
     "neovim/nvim-lspconfig",
     config = function ()
       require "plugins.configs.lspconfig"
