a1ed69b692707ab14c19e64e047afa4824d7d028
DotNaos <schuetzoliver00@gmail.com>
2023-11-05 20:34:25 +0100

    Markdown

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,13 +1,23 @@
 local plugins = { 
 -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
-    {
-      'github/copilot.vim'
-    },
+  {
+    'https://github.com/adelarsq/image_preview.nvim',
+    event = 'VeryLazy',
+    config = function()
+        require("image_preview").setup()
+    end
+  }, 
+  {
+      'github/copilot.vim',
+   },
     {
       'numToStr/Comment.nvim',
-      opts = {
+       config = function ()
+         require('Comment').setup()
+       end,
+       opts = {
         -- add any options here
-      },
+       },
       lazy = false,
     },
     {
